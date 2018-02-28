2a5bb46a98cc99c89291e109395a5d42c63c53e3
KLU01 <31288854+KLU01@users.noreply.github.com>
2018-02-27 23:52:43 -0500

    edit

--- sketch.js
+++ sketch.js
@@ -3,10 +3,12 @@ function setup() {
 }
 
 function draw() {
+  background(255);
+  noStroke();
   if (mouseIsPressed) {
     fill(0);
   } else {
-    fill(255);
+    fill(255, 204, 0);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, 30, 30);
 }
